e193c5843a208614cca9527a64cbdb4a9a235a31
James Fotheringham <james.fotheringham@oracle.com>
2023-08-30 17:15:54 -0700

    Fixing file write issue by creating parent directory first

--- samza-core/src/main/scala/org/apache/samza/util/FileUtil.scala
+++ samza-core/src/main/scala/org/apache/samza/util/FileUtil.scala
@@ -42,6 +42,7 @@ class FileUtil extends Logging {
     var oos: ObjectOutputStream = null
     var fos: FileOutputStream = null
     try {
+      tmpFile.mkdirs()
       fos = new FileOutputStream(tmpFile)
       oos = new ObjectOutputStream(fos)
       oos.writeLong(checksum)
