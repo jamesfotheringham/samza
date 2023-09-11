ea4651f37be09bcddd3e87c07b6fbadbe16f812e
James Fotheringham <james.fotheringham@oracle.com>
2023-09-11 14:29:12 -0700

    code fix

--- samza-core/src/main/scala/org/apache/samza/util/FileUtil.scala
+++ samza-core/src/main/scala/org/apache/samza/util/FileUtil.scala
@@ -42,7 +42,7 @@ class FileUtil extends Logging {
     var oos: ObjectOutputStream = null
     var fos: FileOutputStream = null
     try {
-      tmpFile.getParentFile().mkdirs();
+      tmpFile.getParentFile.mkdirs();
       fos = new FileOutputStream(tmpFile)
       oos = new ObjectOutputStream(fos)
       oos.writeLong(checksum)
